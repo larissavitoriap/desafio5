47604e4f284618a9ae64e3e5d8346a9100576205
larissavitoriap <124819665+larissavitoriap@users.noreply.github.com>
2023-05-03 21:29:17 -0300

    Update FuncionarioResponsavelRepository.java

--- jpa/repositories/FuncionarioResponsavelRepository.java
+++ jpa/repositories/FuncionarioResponsavelRepository.java
@@ -3,5 +3,6 @@ package br.com.edusync.jpa.repositories;
 import br.com.edusync.jpa.Models.FuncionarioResponsavel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface FuncionarioResponsavelRepository extends JpaRepository<FuncionarioResponsavel, Integer> {
 }
