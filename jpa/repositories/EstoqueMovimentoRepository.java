c22a3a53a269a1a7245fbbaa149e4420debd6b17
larissavitoriap <124819665+larissavitoriap@users.noreply.github.com>
2023-05-03 21:30:08 -0300

    Update EstoqueMovimentoRepository.java

--- jpa/repositories/EstoqueMovimentoRepository.java
+++ jpa/repositories/EstoqueMovimentoRepository.java
@@ -2,6 +2,6 @@ package br.com.edusync.jpa.repositories;
 
 import br.com.edusync.jpa.Models.EstoqueMovimento;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface EstoqueMovimentoRepository extends JpaRepository<EstoqueMovimento, Integer> {
 }
